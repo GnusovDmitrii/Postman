c718e3d54706c43c7d2f0007478880a60c57858a
Neto test <gnusov87@yandex.ru>
2026-03-26 16:31:07 +0300

    Initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,39 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldSendPostRequestAndVerifyData() {
+        String testData = "Hello, Postman Echo!";
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body(testData)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(testData));
+    }
+
+    @Test
+    void shouldSendPostRequestWithRussianData() {
+        String testData = "Привет, Postman Echo!";
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body(testData)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(testData));
+    }
+}
